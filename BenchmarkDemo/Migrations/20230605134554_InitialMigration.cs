452ef354ff96963c966e9e118051c10de5394ac6
Kelly E. Osoba <sirkellyc@gmail.com>
2023-06-05 16:40:48 +0100

    Added migration

---
+++ BenchmarkDemo/Migrations/20230605134554_InitialMigration.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BenchmarkDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PasswordSalt = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RefreshToken = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsFalseDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    FalseDeletedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AllowSetNewPassword = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RefreshTokenExpireTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PasswordRecoveryPin = table.Column<int>(type: "int", nullable: false),
+                    RecoveryPinCreationTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserMaxProp",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PasswordSalt = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RefreshToken = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsFalseDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    FalseDeletedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AllowSetNewPassword = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RefreshTokenExpireTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PasswordRecoveryPin = table.Column<int>(type: "int", nullable: false),
+                    RecoveryPinCreationTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserMaxProp", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "UserMaxProp");
+        }
+    }
+}
