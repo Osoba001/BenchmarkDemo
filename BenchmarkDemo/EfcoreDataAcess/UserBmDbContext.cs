a7c3a0405c3dbf4cf3d9a1b7553642df868a9ba6
Kelly E. Osoba <sirkellyc@gmail.com>
2023-06-05 16:44:04 +0100

    Created UserDbContext and Added connectionString to OnMConfiguring

---
+++ BenchmarkDemo/EfcoreDataAcess/UserBmDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BenchmarkDemo.EfcoreDataAcess
+{
+    public class UserBmDbContext:DbContext
+    {
+        //public UserBmDbContext(DbContextOptions<UserBmDbContext> options):base(options)
+        //{
+        //}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=BenchmarkDb;Integrated Security=True; Encrypt=False");
+        }
+
+        public DbSet<User> User { get; set; }
+        public DbSet<UserMaxProp> UserMaxProp { get; set; }
+    }
+}
