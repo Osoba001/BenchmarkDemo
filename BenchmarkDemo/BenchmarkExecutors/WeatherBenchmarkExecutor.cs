5d5be1398378b74fb9cf28954f2d3b8f8f134321
Kelly E. Osoba <sirkellyc@gmail.com>
2023-06-05 11:54:45 +0100

    Created a benchmark on get weather data in get weather forecast method in .Net web api project template

---
+++ BenchmarkDemo/BenchmarkExecutors/WeatherBenchmarkExecutor.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDemo.Controllers;
+using BenchmarkDotNet.Attributes;
+
+namespace BenchmarkDemo.BenchmarkExecutors
+{
+    public class WeatherBenchmarkExecutor
+    {
+        [Benchmark]
+        public void Exeute()
+        {
+            var weatherCtl = new WeatherForecastController(null);
+            weatherCtl.Get();
+        }
+    }
+}
