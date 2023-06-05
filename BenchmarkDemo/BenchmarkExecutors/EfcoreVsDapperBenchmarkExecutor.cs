da9ebb535021fa212a0bd7f4729b335fc52d34f0
Kelly E. Osoba <sirkellyc@gmail.com>
2023-06-05 16:45:47 +0100

    Create a benchmark for both dapper and effcore on fetch all data

---
+++ BenchmarkDemo/BenchmarkExecutors/EfcoreVsDapperBenchmarkExecutor.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDemo.Controllers;
+using BenchmarkDemo.DapperDataAccess;
+using BenchmarkDemo.EfcoreDataAcess;
+using BenchmarkDotNet.Attributes;
+
+namespace BenchmarkDemo.BenchmarkExecutors
+{
+    public class EfcoreVsDapperBenchmarkExecutor
+    {
+        [Benchmark]
+        public void ExecuteEfcore()
+        {
+            var userCtr = new UsersController();
+         var users=   userCtr.GetUser();
+        }[Benchmark]
+        public void ExecuteDapper()
+        {
+            var userCtr = new DataAccess();
+         var users=   userCtr.GetUsers();
+        }
+    }
+}
