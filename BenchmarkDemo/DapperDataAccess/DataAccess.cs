72867c63d1cdc4b6003d7c596edfe4095717c7c4
Kelly E. Osoba <sirkellyc@gmail.com>
2023-06-05 16:44:47 +0100

    Created DataAccess Layer for Dapper Query

---
+++ BenchmarkDemo/DapperDataAccess/DataAccess.cs
@@ -0,0 +1,17 @@
+﻿using BenchmarkDemo.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace BenchmarkDemo.DapperDataAccess
+{
+    public class DataAccess
+    {
+
+        public User[] GetUsers()
+        {
+            using var con = new SqlConnection("Data Source=.;Initial Catalog=BenchmarkDb;Integrated Security=True; Encrypt=False");
+            con.Open();
+            return con.Query<User>(@"select * from [dbo].[User]").ToArray();
+        }
+    }
+}
